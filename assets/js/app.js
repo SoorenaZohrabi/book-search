d74dca7d8f1fcfed117a33a5356f7c858b2ebfad
soorena <soorenazohrabi@gmail.com>
2026-08-11 00:44:10 +0330

    feat: add book search

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
 import { searchBooks } from "./api.js";
 import { renderBooks } from "./render.js";
 
-const data = await searchBooks("javascript");
+const searchInput = document.getElementById("search-input")
+const searchButton = document.getElementById("search-btn")
 
-renderBooks(data.docs);
+async function handleSearch() {
+    const query = searchInput.value.trim();
+
+    if (!query) {
+        return;
+    }
+
+    const data = await searchBooks(query);
+
+    renderBooks(data.docs);
+}
+
+searchButton.addEventListener("click", handleSearch);
